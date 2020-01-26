74bb9016567704198647565fca25bd9d3acadc78
Rajat Borde <borde.rajat@gmail.com>
2020-01-26 15:03:02 +0530

    changes

--- src/main/java/com/helper/MailHelper.java
+++ src/main/java/com/helper/MailHelper.java
@@ -11,20 +11,21 @@ public class MailHelper {
 
     public void send(){
         Email email = EmailBuilder.startingBlank()
-                .from("Rajat Kathuriya","kathuriyarajat@gmail.com")
-                .to("Rajat Kathuriya", "kathuriyarajat@gmail.com")
-                .withReplyTo("Rajat Kathuriya", "kathuriyarajat@gmail.com")
+                .from("Silver Geeker","silvergeeker@gmail.com")
+                .to("Silver Geeker", "silvergeeker@gmail.com")
+                .withReplyTo("Silver Geeker", "silvergeeker@gmail.com")
                 .withSubject("hey")
                 .withPlainText("Please view this email in a modern email client!")
                 .buildEmail();
 
         Mailer mailer = MailerBuilder
-                .withSMTPServer("smtp.gmail.com", 587, "kathuriyarajat@gmail.com", "Rajat@5692")
+                .withSMTPServer("smtp.gmail.com", 587, "silvergeeker@gmail.com", "silver96")
                 .withTransportStrategy(TransportStrategy.SMTP_TLS)
                 //.withProxy("172.31.100.29", 3128, "edcguest", "edcguest")
                 .withSessionTimeout(10 * 1000)
                 .withProperty("mail.smtp.auth", "true")
                 .withProperty("mail.smtp.starttls.enable","true")
+                //.withProperty( "enable_starttls_auto","true")
                 .withDebugLogging(true)
                 .buildMailer();
 
